1d693d99f6e712e92ed76f515236199038a9486f
Akshit Kumar Gautam <akshitkumargautam@gmail.com>
2021-11-25 00:11:23 +0530

    created my very first nodejs application

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
-function changeImage(){
-    document.getElementById('imaged').innerHTML=image.src('assets/images/typing.png');
-}
+var http= require('http');
+http.createServer(function (request, response) {
+    response.writeHead(200, {'Content-Type':'text/plain'});
+
+    response.end('Hello, World!\n');
+}).listen(8081);
+
+console.log('Server Running at http://127.0.0.1:8081/');
+
+// I was required to enter the http url that I have mentioned in the console.log statement manually into the browser
